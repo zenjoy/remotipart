7092d4fb1c3ae075fff05358d5943c0c997b9f60
M.Shibuya <mit.shibuya@gmail.com>
2019-06-20 15:14:08 +0900

    Use Module#prepend if available for ActiveScaffold compatibility. Fixes #202

--- spec/dummy_app/config/routes.rb
+++ spec/dummy_app/config/routes.rb
@@ -2,5 +2,6 @@ DummyApp::Application.routes.draw do
   match 'comments' => 'comments#create', :via => [:put]
   match 'say' => 'comments#say', :via => [:get]
   resources :comments
+  match 'prepended' => 'prepended#show', :via => [:get]
   root :to => "comments#index"
 end
